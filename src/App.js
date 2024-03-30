697c72c09f210758b080febf5bb4437bdbbf19b0
James <james_lieb@hotmail.com>
2024-03-29 22:31:26 -0400

    wrote index javascript, added styling to base page

--- src/App.js
+++ src/App.js
@@ -1,1 +1,6 @@
+import { useState, useEffect } from "react";
+
+export default function Search() {
+    
+}
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { createRoot } from 'react-dom/client';
+import "./styles.css";
 
+import App from "./App.js";
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
